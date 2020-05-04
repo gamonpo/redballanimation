06ba06345d32bcd1ccbc73037bc77f38bfeca203
gamonpo <gamonpo@outlook.com>
2020-05-04 18:00:03 -0300

    Animated with panresponder

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {Component} from 'react';
-import {StyleSheet, View, Animated} from 'react-native';
+import {StyleSheet, View, Animated, PanResponder} from 'react-native';
 
 export default class App extends Component {
   state = {
-    ballY: new Animated.Value(0),
+    ball: new Animated.ValueXY({x: 0, y: 0}),
   };
 
-  componentDidMount() {
-    Animated.timing(this.state.ballY, {
-      toValue: 500,
-      duration: 1000,
-    }).start();
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: (e, gestureState) => true,
+
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.ball.setOffset({
+          x: this.state.ball.x._value,
+          y: this.state.ball.y._value,
+        });
+
+        this.state.ball.setValue({x: 0, y: 0});
+      },
+
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {
+            dx: this.state.ball.x,
+            dy: this.state.ball.y,
+          },
+        ],
+        {
+          listener: (e, gestureState) => {
+            console.log(gestureState);
+          },
+        },
+      ),
+      onPanResponderRelease: () => {
+        this.state.ball.flattenOffset();
+      },
+    });
   }
 
   render() {
@@ -26,15 +44,15 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Animated.View
+          {...this._panResponder.panHandlers}
           style={[
             styles.ball,
+
             {
-              top: this.state.ballY,
-              opacity: this.state.ballY.interpolate({
-                inputRange: [0, 300],
-                outputRange: [1, 0.2],
-                extrapolate: 'clamp',
-              }),
+              transform: [
+                {translateX: this.state.ball.x},
+                {translateY: this.state.ball.y},
+              ],
             },
           ]}
         />
